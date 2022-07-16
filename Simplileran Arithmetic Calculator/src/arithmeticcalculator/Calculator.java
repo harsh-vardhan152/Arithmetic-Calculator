f825d519139523546b7f40d128a7d976897ea125
Harsh Vardhan <54477764+harsh-vardhan152@users.noreply.github.com>
2022-07-16 16:15:14 +0530

    Full calculator code is done

--- Simplileran Arithmetic Calculator/src/arithmeticcalculator/Calculator.java
+++ Simplileran Arithmetic Calculator/src/arithmeticcalculator/Calculator.java
@@ -6,14 +6,42 @@ public class Calculator {
 	
 	public static void main(String[] args) {
 		
-		double num1,num2;
+		double num1,num2,result;
 		int op;
 		
-		System.out.println("Enter the number");
+		System.out.println("Enter two number");
 		Scanner sc = new Scanner(System.in);
 		num1=sc.nextDouble();
 		num2=sc.nextDouble(); 
-		System.out.println("Select the operations");
+		System.out.println("Select the operations from \n1. Addtion\n2. Subtraction\n3. Multiplication\n4. Division");
+		op= sc.nextInt();
+		
+		if(op==1)
+		{
+			result= num1+num2;
+			System.out.println("Addition "+result);
+		}
+		else if(op==2)
+		{
+			result= num1-num2;
+			System.out.println("Subtraction "+result);
+		}
+		else if(op==3) 
+		{
+			result= num1*num2;
+			System.out.println("Multiplication "+result);
+		}
+		else if(op==4)
+			{
+			result= num1/num2;
+			System.out.println("Division "+result);	
+			}
+		else {
+			System.out.println("Inavlid opertions is Selected/Invalid Request");
+		}
 	}
+
+	
+		
 
 }
