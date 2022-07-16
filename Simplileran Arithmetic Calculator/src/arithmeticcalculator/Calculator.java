e919dd6e03ed50ac97bf7a47a8f7b485ec53cb6b
Harsh Vardhan <54477764+harsh-vardhan152@users.noreply.github.com>
2022-07-16 16:20:18 +0530

    Writing comment for proper clarification

--- Simplileran Arithmetic Calculator/src/arithmeticcalculator/Calculator.java
+++ Simplileran Arithmetic Calculator/src/arithmeticcalculator/Calculator.java
@@ -1,18 +1,23 @@
 package arithmeticcalculator;
 
+// Importing Scanner class for taking user input 
 import java.util.Scanner;
 
 public class Calculator {
 	
 	public static void main(String[] args) {
 		
+		// Deceleration of variables
 		double num1,num2,result;
 		int op;
 		
+		// Creating scanner object for taking input from user
 		System.out.println("Enter two number");
 		Scanner sc = new Scanner(System.in);
 		num1=sc.nextDouble();
 		num2=sc.nextDouble(); 
+		
+		// Selecting the operations that should be performed with this calculator 
 		System.out.println("Select the operations from \n1. Addtion\n2. Subtraction\n3. Multiplication\n4. Division");
 		op= sc.nextInt();
 		
@@ -36,12 +41,9 @@ public class Calculator {
 			result= num1/num2;
 			System.out.println("Division "+result);	
 			}
+		// if user input invalid number then this line of code is executed 
 		else {
 			System.out.println("Inavlid opertions is Selected/Invalid Request");
 		}
 	}
-
-	
-		
-
 }
