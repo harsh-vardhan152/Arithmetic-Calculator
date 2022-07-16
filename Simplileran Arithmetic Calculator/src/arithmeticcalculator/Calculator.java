b2cc1df4c10e1310988696ab6cb514bb79c658a0
Harsh Vardhan <54477764+harsh-vardhan152@users.noreply.github.com>
2022-07-16 16:00:59 +0530

    Created scanner class and scanner object and also created package,class

---
+++ Simplileran Arithmetic Calculator/src/arithmeticcalculator/Calculator.java
@@ -0,0 +1,19 @@
+package arithmeticcalculator;
+
+import java.util.Scanner;
+
+public class Calculator {
+	
+	public static void main(String[] args) {
+		
+		double num1,num2;
+		int op;
+		
+		System.out.println("Enter the number");
+		Scanner sc = new Scanner(System.in);
+		num1=sc.nextDouble();
+		num2=sc.nextDouble(); 
+		System.out.println("Select the operations");
+	}
+
+}
