cfffcc5ce2a90d0b6e815c3c52eff91f64ce6dd5
Harsh Vardhan <54477764+harsh-vardhan152@users.noreply.github.com>
2022-07-18 14:41:20 +0530

    writing program using this and methods

--- Simplileran Arithmetic Calculator/src/arithmeticcalculator/Calculator.java
+++ Simplileran Arithmetic Calculator/src/arithmeticcalculator/Calculator.java
@@ -1,49 +1,71 @@
 package arithmeticcalculator;
 
 // Importing Scanner class for taking user input 
-import java.util.Scanner;
+import java.util.*;
 
 public class Calculator {
-	
-	public static void main(String[] args) {
-		
-		// Deceleration of variables
-		double num1,num2,result;
-		int op;
-		
-		// Creating scanner object for taking input from user
-		System.out.println("Enter two number");
-		Scanner sc = new Scanner(System.in);
-		num1=sc.nextDouble();
-		num2=sc.nextDouble(); 
-		
-		// Selecting the operations that should be performed with this calculator 
-		System.out.println("Select the operations from \n1. Addtion\n2. Subtraction\n3. Multiplication\n4. Division");
-		op= sc.nextInt();
-		
-		if(op==1)
+	static int n1, n2;
+	Calculator(int n1,int n2){
+		this.n1=n1;
+		this.n2=n2;
+	}
+	Scanner sc = new Scanner(System.in);
+	int addition()
+	{
+		System.out.println("Enter first number for addition");
+		n1=sc.nextInt();
+		System.out.println("Enter second number for addition");
+		n2=sc.nextInt();
+		return n1+n2;
+	}
+	int subtraction()
+	{
+		System.out.println("Enter first numbers for subtraction");
+		n1=sc.nextInt();
+		System.out.println("Enter second numbers for subtraction");
+		n2=sc.nextInt();
+		return n1-n2;
+	}
+	int multiplication()
+	{
+		System.out.println("Enter first numbers for multiply");
+		n1=sc.nextInt();
+		System.out.println("Enter second numbers for multiply");
+		n2=sc.nextInt();
+		return n1*n2;
+	}
+	int division()
+	{
+		System.out.println("Enter first numbers for divide");
+		n1=sc.nextInt();
+		System.out.println("Enter second numbers for divide");
+		n2=sc.nextInt();
+		return(n1/n2);
+	}
+	public static void main(String[] args)
+	{
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter the operation that you want to perform by writing the correct form as mention below:- \n1. Addition\n2. Subtraction\n3. Multiplication\n4. Division");
+		String str = s.nextLine();
+		Calculator obj = new Calculator(n1,n2);
+		int n=0;
+		switch(str)
 		{
-			result= num1+num2;
-			System.out.println("Addition "+result);
+		case "Addition":
+			n=obj.addition();
+			break;
+		case "Subtraction":
+			n=obj.subtraction();
+			break;
+		case "Multiplication":
+			n=obj.multiplication();
+			break;
+		case "Division":
+			n=obj.division();
+			break;
+		default:
+			System.out.println("Wrong operations selected or Character don't match with the input");
 		}
-		else if(op==2)
-		{
-			result= num1-num2;
-			System.out.println("Subtraction "+result);
-		}
-		else if(op==3) 
-		{
-			result= num1*num2;
-			System.out.println("Multiplication "+result);
-		}
-		else if(op==4)
-			{
-			result= num1/num2;
-			System.out.println("Division "+result);	
-			}
-		// if user input invalid number then this line of code is executed 
-		else {
-			System.out.println("Inavlid opertions is Selected/Invalid Request");
-		}
+		System.out.println("Answer = "+n);
 	}
 }
